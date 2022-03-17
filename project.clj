f4bbb6d09e99c9e410c0057a9c4152c4f710d94b
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-03-17 13:33:16 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject com.ambrosebs/example "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.logic "1.0.0"]])
+                 [org.clojure/core.logic "1.0.1"]])
