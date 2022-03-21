349a173b153c5eb9b52a7fbfc4073090f08c0dd2
Wanderson Ferreira <wand@hey.com>
2022-03-21 13:52:27 -0300

    test

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject com.ambrosebs/example "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.logic "1.0.0"]])
+                 [org.clojure/core.logic "1.0.0"]
+                 [hikari-cp/hikari-cp "2.12.0"]])
